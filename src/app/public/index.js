fedccabc38cff4fb67e0a8f28b104da769f497c3
Tarsis <br.tmvdl@gmail.com>
2024-08-19 11:55:44 -0300

    Updating app files

--- src/app/public/index.js
+++ src/app/public/index.js
@@ -1,8 +1,25 @@
-import { HTML } from '@brtmvdl/frontend'
+import { HTML, nButton } from '@brtmvdl/frontend'
+import { FormComponent } from './components/form.component.js'
+import { InputComponent } from './components/input.component.js'
 
 export class Page extends HTML {
   onCreate() {
     super.onCreate()
-    this.setText('page')
+    this.append(this.getForm())
+  }
+
+  getForm() {
+    const form = new FormComponent({ action: 'image' })
+    form.append(new InputComponent({ label: 'width', value: '400' }))
+    form.append(new InputComponent({ label: 'height', value: '400' }))
+    form.append(new InputComponent({ label: 'text', value: 'image' }))
+    form.append(this.createButton({ text: 'create' }))
+    return form
+  }
+
+  createButton({ text = null } = {}) {
+    const button = new nButton()
+    button.setText(text)
+    return button
   }
 }
