c9acb80880ed880c409fd1ef430d770c21819fe9
Tarsis <br.tmvdl@gmail.com>
2024-08-26 17:55:24 -0300

    Updating app files

--- src/nodejs/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const { port } = require('./config.js')
-
-app.use(express.static('public'))
-
-app.get('/image', (req, res) => res.sendFile('/data/image.png'))
-
-app.listen(port, () => console.log(`Listening on port ${port}`))
